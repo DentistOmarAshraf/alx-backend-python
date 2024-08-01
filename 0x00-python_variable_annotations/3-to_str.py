d5930d621d6fd51b0b3fbe4f4f79fafcccffeaf8
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-01 14:37:11 +0300

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+to_str - take a float and return string of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    n - float
+    return string (stringify float)
+    """
+    return str(n)
