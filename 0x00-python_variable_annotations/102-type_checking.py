500451652a8d8a98045d19f01dbf9bf7e1495b54
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-01 20:27:42 +0300

    pycode

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """
-zoom_array-
+zoom_array - duplicate entry tuple
 """
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    lst: tuple
+    factor: int , default = 2
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
