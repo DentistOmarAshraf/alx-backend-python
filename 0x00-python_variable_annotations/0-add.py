d184b0179ed71438b478cb6a8dc25f0352834df7
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-01 14:23:45 +0300

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Add - adding two floats and return float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    a: float
+    b: float
+    return float
+    """
+    return a + b
