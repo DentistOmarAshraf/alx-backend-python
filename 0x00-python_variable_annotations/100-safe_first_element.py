77d4d6f5d7e0c7f84bdc17e7b0f9a80125ebf35b
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-01 19:50:42 +0300

    safe_first_element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+safe_first_elemet: accept list and reutrn union(any, None)
+"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
+    
