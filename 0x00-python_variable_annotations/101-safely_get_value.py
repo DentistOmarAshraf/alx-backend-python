2bac5c2f6a0a6619e910fcbe78cedd7d2c6bf0f5
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-01 20:04:47 +0300

    101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+safely_get_value - accept dictionary and return a dict[key] else default
+"""
+from typing import TypeVar, Mapping, Any, Union
+
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    dct: dictionary (Mapping)
+    key: (any)
+    dafault: variale type T or None
+    return T type or any
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
