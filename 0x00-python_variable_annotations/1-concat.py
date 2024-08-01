43333f84b14333ab8b49aee54fc3634531b0b975
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-01 14:27:00 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+concat function - connect tow string
+return string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    str1 - string
+    str2 - string
+    return string
+    """
+    return "{}{}".format(str1, str2)
