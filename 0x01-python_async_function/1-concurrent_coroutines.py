90ab930fc06508fd78576a1a918ca2e585cd486c
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-06 05:44:04 +0300

    -----

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,6 +2,7 @@
 """
 Asyncio
 """
+
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -16,4 +17,4 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     for _ in range(n):
         list_return.append(await wait_random(max_delay))
 
-    return list_return
+    return list_return.sort()
