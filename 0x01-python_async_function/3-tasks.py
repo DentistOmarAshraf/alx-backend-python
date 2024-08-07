67a02e87c5529bce0f9c497471e7556cd3e0128e
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-07 14:30:22 +0300

    3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> Awaitable[float]:
     """
     max_delay: int
     return asyncio Task
