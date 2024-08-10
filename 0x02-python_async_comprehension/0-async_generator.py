38513394ab4dca021339867bf76f906ca8d62b18
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-10 18:56:38 +0300

    ---

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,16 +3,16 @@
 Async generator
 """
 
+import asyncio
 import random
 from typing import Generator
-import time
 
 
-def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     AsyncIO Generator
     """
     for _ in range(10):
         num: float = random.uniform(0, 10)
-        time.sleep(1)
+        await asyncio.sleep(1)
         yield num
