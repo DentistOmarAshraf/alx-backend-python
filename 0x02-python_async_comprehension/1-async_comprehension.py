6f82028908389e11f0bb6996dde56145d69361f4
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-10 19:40:59 +0300

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,13 @@
 Asyncio Comperhension
 """
 
+from typing import List
+
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """
     Loop over asyncio generator and reuturn list of fload (results)
     """
