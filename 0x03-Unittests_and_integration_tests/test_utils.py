f3139d0fd2a26a2976f0e57d5c98ad81b3c88c6f
DentistOmarAshraf <omar_ashraf@live.com>
2024-09-15 19:06:42 +0300

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+utils testing
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from typing import Mapping, Sequence
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Testing access_nested_map
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a":{"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               output: Mapping | int) -> None:
+        self.assertEqual(access_nested_map(nested_map, path), output)
+
+
+if __name__ == "__main__":
+    unittest.main()
